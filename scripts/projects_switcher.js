9111f5f13afa34be437fb41e23858ef231b54243
DKhorkov <dkhorkov.dev@gmail.com>
2023-05-15 21:45:37 +0300

    added project  buttons animation

--- scripts/projects_switcher.js
+++ scripts/projects_switcher.js
@@ -32,6 +32,14 @@ export default class ProjectSwitvher {
   #djangoWebstoreChanger() {
     this.#telegram_bot_info.style.marginLeft = '-80%';
     this.#telegram_bot_info.style.transition = 'all 1s ease';
+    this.#django_webstore_button.style.background = 'black';
+    this.#django_webstore_button.style.cursor = 'auto';
+    this.#fast_api_task_manager_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
+    this.#fast_api_task_manager_button.style.backgroundSize = '400%';
+    this.#fast_api_task_manager_button.style.cursor = 'pointer';
+    this.#telegram_bot_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
+    this.#telegram_bot_button.style.backgroundSize = '400%';
+    this.#telegram_bot_button.style.cursor = 'pointer';
 
     // if (this.#django_webstore_info.style.backgroundColor == 'red') {
     //   this.#django_webstore_info.style.backgroundColor = 'white';
@@ -49,10 +57,26 @@ export default class ProjectSwitvher {
   #telegramBotChanger() {
     this.#telegram_bot_info.style.marginLeft = '10%';
     this.#telegram_bot_info.style.transition = 'all 1s ease';
+    this.#telegram_bot_button.style.background = 'black';
+    this.#telegram_bot_button.style.cursor = 'auto';
+    this.#django_webstore_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
+    this.#django_webstore_button.style.backgroundSize = '400%';
+    this.#django_webstore_button.style.cursor = 'pointer';
+    this.#fast_api_task_manager_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
+    this.#fast_api_task_manager_button.style.backgroundSize = '400%';
+    this.#fast_api_task_manager_button.style.cursor = 'pointer';
   }
 
   #fastApiTaskManagerChanger() {
     this.#telegram_bot_info.style.marginLeft = '-170%';
     this.#telegram_bot_info.style.transition = 'all 1s ease';
+    this.#fast_api_task_manager_button.style.background = 'black';
+    this.#fast_api_task_manager_button.style.cursor = 'auto';
+    this.#django_webstore_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
+    this.#django_webstore_button.style.backgroundSize = '400%';
+    this.#django_webstore_button.style.cursor = 'pointer';
+    this.#telegram_bot_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
+    this.#telegram_bot_button.style.backgroundSize = '400%';
+    this.#telegram_bot_button.style.cursor = 'pointer';
   }
 }
