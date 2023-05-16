a2a908bd726a23abd84a39b49f6bd33de5151ad4
DKhorkov <dkhorkov.dev@gmail.com>
2023-05-16 22:07:47 +0300

    overrided scripts to switch projects, marked project div

--- scripts/projects_switcher.js
+++ scripts/projects_switcher.js
@@ -1,82 +1,46 @@
 export default class ProjectSwitvher {
-  #telegram_bot_button;
-  #django_webstore_button;
-  #fast_api_task_manager_button;
-
+  #buttons;
+  #active_button;
   #telegram_bot_info;
-  #django_webstore_info;
-  #fast_api_task_manager_info;
 
   constructor() {
-    this.#telegram_bot_button = document.getElementById('telegram-bot-button');
-    this.#django_webstore_button = document.getElementById('django-webstore-button');
-    this.#fast_api_task_manager_button = document.getElementById('fast-api-task-manager-button');
-
+    this.#buttons = document.querySelectorAll('.projects-button'); // находим все объекты с определенным классом
+    this.#active_button = this.#buttons[0]; // активная кнопка со старта
     this.#telegram_bot_info = document.getElementById('telegram-bot-info');
-    this.#django_webstore_info = document.getElementById('django-webstore-info');
-    this.#fast_api_task_manager_info = document.getElementById('fast-api-task-manager-info');
   }
 
   changeProjectMonitor() {
-    /* 
-        ОБЯЗАТЕЛЬНО bind, ИНАЧЕ БУДЕТ ОШИБКА, метод не понимает, что за контекст мы ему скармливаем в EventListener
+    // итерируем по всем кнопкам с найденным классом и навешиваем addEventListener
+    this.#buttons.forEach((button) => {
+      button.addEventListener('click', this.#handleBurronClick.bind(this));
+    });
+  }
 
-        Либо можно заменить стрелочной функцией. Считается более новым решением. Но тут много изменений стилей, поэтмоу bind
+  #handleBurronClick(event) {
+    /*
+      Удаляем у текущей активной кнопки класс active и навешиваем его на кнопку, которую нажали. 
+      Плюс заменяем нажатую кнопку в памяти
     */
+    this.#active_button.classList.remove('active');
+    event.target.classList.add('active');
+    this.#active_button = event.target;
 
-    this.#telegram_bot_button.addEventListener('click', this.#telegramBotChanger.bind(this));
-    this.#django_webstore_button.addEventListener('click', this.#djangoWebstoreChanger.bind(this));
-    this.#fast_api_task_manager_button.addEventListener('click', this.#fastApiTaskManagerChanger.bind(this));
+    // Двигаем наши контейнеры с проектами
+    if (event.target.innerHTML == 'PlantsCareBot') {
+      this.#telegram_bot_info.style.cssText = this.#prepareCssText({ marginLeftInPercent: 10 }); // синтаксис kwargs
+    } else if (event.target.innerHTML == 'NataliaToys') {
+      this.#telegram_bot_info.style.cssText = this.#prepareCssText({ marginLeftInPercent: -80 });
+    } else {
+      this.#telegram_bot_info.style.cssText = this.#prepareCssText({ marginLeftInPercent: -170 });
+    }
   }
 
-  #djangoWebstoreChanger() {
-    this.#telegram_bot_info.style.marginLeft = '-80%';
-    this.#telegram_bot_info.style.transition = 'all 1s ease';
-    this.#django_webstore_button.style.background = 'black';
-    this.#django_webstore_button.style.cursor = 'auto';
-    this.#fast_api_task_manager_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
-    this.#fast_api_task_manager_button.style.backgroundSize = '400%';
-    this.#fast_api_task_manager_button.style.cursor = 'pointer';
-    this.#telegram_bot_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
-    this.#telegram_bot_button.style.backgroundSize = '400%';
-    this.#telegram_bot_button.style.cursor = 'pointer';
+  #prepareCssText(options) {
+    const cssText = `
+      margin-left: ${options.marginLeftInPercent}%;
+      transition: all 1s ease;
+    `;
 
-    // if (this.#django_webstore_info.style.backgroundColor == 'red') {
-    //   this.#django_webstore_info.style.backgroundColor = 'white';
-    // } else {
-    //   this.#django_webstore_info.style.cssText = newStyle;
-    // }
-
-    // const newStyle = `
-    //   background-color: red;
-    //   border: 10px solid black;
-    // `;
-    // this.#django_webstore_info.style.cssText = newStyle;
-  }
-
-  #telegramBotChanger() {
-    this.#telegram_bot_info.style.marginLeft = '10%';
-    this.#telegram_bot_info.style.transition = 'all 1s ease';
-    this.#telegram_bot_button.style.background = 'black';
-    this.#telegram_bot_button.style.cursor = 'auto';
-    this.#django_webstore_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
-    this.#django_webstore_button.style.backgroundSize = '400%';
-    this.#django_webstore_button.style.cursor = 'pointer';
-    this.#fast_api_task_manager_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
-    this.#fast_api_task_manager_button.style.backgroundSize = '400%';
-    this.#fast_api_task_manager_button.style.cursor = 'pointer';
-  }
-
-  #fastApiTaskManagerChanger() {
-    this.#telegram_bot_info.style.marginLeft = '-170%';
-    this.#telegram_bot_info.style.transition = 'all 1s ease';
-    this.#fast_api_task_manager_button.style.background = 'black';
-    this.#fast_api_task_manager_button.style.cursor = 'auto';
-    this.#django_webstore_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
-    this.#django_webstore_button.style.backgroundSize = '400%';
-    this.#django_webstore_button.style.cursor = 'pointer';
-    this.#telegram_bot_button.style.background = 'linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4)';
-    this.#telegram_bot_button.style.backgroundSize = '400%';
-    this.#telegram_bot_button.style.cursor = 'pointer';
+    return cssText;
   }
 }
