65588e12a44e04c7cc35420c853738f4b1852c25
DKhorkov <dkhorkov.dev@gmail.com>
2023-05-19 21:50:49 +0300

    Added previous photo logics, finished next photo logics

--- scripts/photoes_switcher.js
+++ scripts/photoes_switcher.js
@@ -1,17 +1,33 @@
 export default class photoesSwtitcher {
+  #target_ids;
+
   #next_buttons;
   #previous_buttons;
 
   #tg_bot_photoes_list;
   #tg_bot_current_index;
 
+  #django_webstore_photoes_list;
+  #django_webstore_current_index;
+
+  #fast_api_task_manager_photoes_list;
+  #fast_api_task_manager_current_index;
+
   constructor() {
+    this.#target_ids = ['telegram-bot-photoes', 'django-webstore-photoes', 'fast-api-task-manager-photoes'];
+
     this.#next_buttons = document.querySelectorAll('.next_photo');
     this.#previous_buttons = document.querySelectorAll('.previous_photo');
 
     // ... - распаковка объекта по ключам. [] - что-то вроде  list comprehension
     this.#tg_bot_photoes_list = [...Array(4).keys()];
     this.#tg_bot_current_index = 0;
+
+    this.#django_webstore_photoes_list = [...Array(4).keys()];
+    this.#django_webstore_current_index = 0;
+
+    this.#fast_api_task_manager_photoes_list = [...Array(4).keys()];
+    this.#fast_api_task_manager_current_index = 0;
   }
 
   changePhotoes() {
@@ -19,19 +35,34 @@ export default class photoesSwtitcher {
       button.addEventListener('click', this.#nextPhoto.bind(this));
     });
 
-    // this.#previous_buttons.forEach((button) => {
-    //   button.addEventListener('click', this.#previousPhoto.bind(this));
-    // });
+    this.#previous_buttons.forEach((button) => {
+      button.addEventListener('click', this.#previousPhoto.bind(this));
+    });
   }
 
   #nextPhoto(event) {
-    const target_ids = ['telegram-bot-photoes'];
-    if (event.target.parentNode.parentNode.id == target_ids[0]) {
-      this.#tg_bot_current_index = this.#changePhoroToNextFromList(
-        this.#tg_bot_photoes_list,
-        this.#tg_bot_current_index,
-        target_ids[0]
-      );
+    switch (event.target.parentNode.parentNode.id) {
+      case this.#target_ids[0]:
+        this.#tg_bot_current_index = this.#changePhoroToNextFromList(
+          this.#tg_bot_photoes_list,
+          this.#tg_bot_current_index,
+          this.#target_ids[0]
+        );
+        break;
+      case this.#target_ids[1]:
+        this.#django_webstore_current_index = this.#changePhoroToNextFromList(
+          this.#django_webstore_photoes_list,
+          this.#django_webstore_current_index,
+          this.#target_ids[1]
+        );
+        break;
+      case this.#target_ids[2]:
+        this.#fast_api_task_manager_current_index = this.#changePhoroToNextFromList(
+          this.#fast_api_task_manager_photoes_list,
+          this.#fast_api_task_manager_current_index,
+          this.#target_ids[2]
+        );
+        break;
     }
   }
 
@@ -49,5 +80,50 @@ export default class photoesSwtitcher {
     document.getElementById(target_id).style.backgroundImage = `url('../images/${target_id}/${next_photo_index}.png')`;
 
     return next_photo_index;
+  }
+
+  #previousPhoto(event) {
+    switch (event.target.parentNode.parentNode.id) {
+      case this.#target_ids[0]:
+        this.#tg_bot_current_index = this.#changePhoroToPreviousFromList(
+          this.#tg_bot_photoes_list,
+          this.#tg_bot_current_index,
+          this.#target_ids[0]
+        );
+        break;
+      case this.#target_ids[1]:
+        this.#django_webstore_current_index = this.#changePhoroToPreviousFromList(
+          this.#django_webstore_photoes_list,
+          this.#django_webstore_current_index,
+          this.#target_ids[1]
+        );
+        break;
+      case this.#target_ids[2]:
+        this.#fast_api_task_manager_current_index = this.#changePhoroToPreviousFromList(
+          this.#fast_api_task_manager_photoes_list,
+          this.#fast_api_task_manager_current_index,
+          this.#target_ids[2]
+        );
+        break;
+    }
+  }
+
+  #changePhoroToPreviousFromList(photoes_list, current_photo_index, target_id) {
+    let previous_photo_index;
+    try {
+      previous_photo_index = photoes_list[--current_photo_index];
+      if (previous_photo_index == undefined) {
+        throw Error;
+      }
+    } catch (error) {
+      // Тут нужна именно распаковка для корректной работы метода max() модуля Math
+      previous_photo_index = Math.max(...photoes_list);
+    }
+
+    document.getElementById(
+      target_id
+    ).style.backgroundImage = `url('../images/${target_id}/${previous_photo_index}.png')`;
+
+    return previous_photo_index;
   }
 }
